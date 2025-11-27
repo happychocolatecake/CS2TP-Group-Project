19c89c330d811a24e3aab1084e7613c53a827e80
Samuel D'souza <samuel_dsouza@outlook.com>
2025-11-27 22:06:10 +0000

    Initial commit of Livewire/Laravel rewrite

---
+++ resources/views/cc.blade.php
@@ -0,0 +1,3 @@
+<x-layout>
+    <h1>Hello from the Component Chooser.</h1>
+</x-layout>
---
+++ routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Laravel\Fortify\Features;
+use Livewire\Volt\Volt;
+
+Route::get('/', function () {
+    return view('index');
+})->name('home');
+
+Route::get('/store', function () {
+    return view('store');
+})->name('home');
+
+Route::get('/cc', function () {
+    return view('cc');
+})->name('home');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('home');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('home');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::middleware(['auth'])->group(function () {
+    Route::redirect('settings', 'settings/profile');
+
+    Volt::route('settings/profile', 'settings.profile')->name('profile.edit');
+    Volt::route('settings/password', 'settings.password')->name('user-password.edit');
+    Volt::route('settings/appearance', 'settings.appearance')->name('appearance.edit');
+
+    Volt::route('settings/two-factor', 'settings.two-factor')
+        ->middleware(
+            when(
+                Features::canManageTwoFactorAuthentication()
+                    && Features::optionEnabled(Features::twoFactorAuthentication(), 'confirmPassword'),
+                ['password.confirm'],
+                [],
+            ),
+        )
+        ->name('two-factor.show');
+});
