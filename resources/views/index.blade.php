9e8de4165da5e93f11cc3559c14a71505b436a52
Zain Khan <zainkhan24681@gmail.com>
2025-11-29 13:29:51 +0000

    Added hero section to homepage

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,35 @@
 <x-header></x-header>
+
 <x-layout>
-    <h1>Hello from the Home Page.</h1>
+
+    <div class="grid grid-cols-1 md:grid-cols-2 bg-gray-200 mb-12">
+
+
+    <div class="h-96 overflow-hidden">
+        <img src="/images/cool_pc.jpg" alt="Cool PC" class="w-full h-full object-cover">
+    </div>
+
+    <div class="p-12 flex flex-col justify-center">
+
+    <h1 class="text-4xl font-bold mb-4 text-gray-900">
+        "Power Your Dreams, One Build at a Time. "
+    </h1>
+
+    <p class="text-gray-600 mb-8">
+            We offer the best prices on CPUs, GPUs and pre-builts. Better service, better gaming.
+    </p>
+
+    <div class="flex gap-4">
+        <a href="/store" class="bg-gray-900 text-white px-6 py-3 rounded hover:bg-gray-700">
+            Shop Now
+        </a>
+        <a href="/cc" class="bg-gray-700 text-white px-6 py-3 rounded hover:bg-gray-600">
+            Computer Configurator
+        </a>
+    </div>
+    </div>
+    </div>
+
 </x-layout>
+
 <x-footer></x-footer>
