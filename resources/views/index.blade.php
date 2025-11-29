88df1c8b0a0657e21eea197b0534b05dcf7085d2
Zain Khan <zainkhan24681@gmail.com>
2025-11-29 15:53:54 +0000

    Completed entire homepage design

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -61,6 +61,74 @@
 
     </div>
 
+    <div class="bg-gray-100 py-12 mb-16">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+
+            <h2 class="text-3xl font-bold text-center mb-10">Best Sellers</h2>
+
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
+
+                <x-product-card
+                    title="Popular Pre-Built"
+                    description="Ready to game straight out the box."
+                    price="£1300"
+                    />
+
+                <x-product-card
+                    title="Popular CPU"
+                    description="High pefformance CPU for gaming and productivity."
+                    price="£300"
+                    />
+
+                <x-product-card
+                    title="Popular GPU"
+                    description="Next-gen graphics power."
+                    price="£600"
+                    />
+
+            </div>
+        </div>
+    </div>
+
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mb-20 text-center">
+
+        <h2 class="text-3xl font-bold mb-4">Not sure where to start?</h2>
+        <p class="text-gray-600 mb-12">Save your time of researching, and use our resources to configure your computer.</p>
+
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-10">
+
+        <div class="flex flex-col item-center">
+            <div class="bg-gray-100 p-6 rounded-2xl mb-4 flex items-center justify-center">
+                <img src="/images/box.png" alt="Bundles Icon" class="w-10 h-10">
+            </div>
+            <p class="bg-gray-100 p-2 px-4 rounded text-sm">Purchase Bundles, chosen by our experts</p>
+        </div>
+
+        <div class="flex flex-col item-center">
+            <div class="bg-gray-100 p-6 rounded-2xl mb-4 flex items-center justify-center">
+                <img src="/images/refresh.png" alt="Configurator Icon" class="w-10 h-10">
+            </div>
+            <p class="bg-gray-100 p-2 px-4 rounded text-sm">Configure and check for compactibility to build your dream PC</p>
+        </div>
+
+        <div class="flex flex-col item-center">
+            <div class="bg-gray-100 p-6 rounded-2xl mb-4 flex items-center justify-center">
+                <img src="/images/search.png" alt="Guides Icon" class="w-10 h-10">
+            </div>
+            <p class="bg-gray-100 p-2 px-4 rounded text-sm">To learn the essentials of building a PC, read our guides.</p>
+        </div>
+
+    </div>
+
+    <div class="mt-12">
+        <a href="/cc" class="bg-gray-800 text-white px-8 py-3 rounded hover:bg-gray-700 transition">
+            Start Now
+        </a>
+    </div>
+
+
+
+
 
 
 
