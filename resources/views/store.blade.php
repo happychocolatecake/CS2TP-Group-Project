c096da44986d4b1dbfccfc3ee580abe7d5308ab6
Samuel D'souza <samuel_dsouza@outlook.com>
2025-11-29 12:00:34 +0000

    added product cards

--- resources/views/store.blade.php
+++ resources/views/store.blade.php
@@ -2,4 +2,8 @@
 <x-layout>
     <h1>Hello from the Store Page.</h1>
 </x-layout>
+<x-product-card 
+          title="Popular Pre-Built" 
+          description="High performance gaming rig." 
+          price="$1200"/>
 <x-footer></x-footer>
