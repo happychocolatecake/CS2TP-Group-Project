77564d904b2b09b3ba616bd4632980d57187a932
Samuel D'souza <samuel_dsouza@outlook.com>
2025-11-29 15:38:46 +0000

    created and organised the layout for multiple store cards

---
+++ resources/views/components/product-card-row.blade.php
@@ -0,0 +1,21 @@
+<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 p-6">
+    <x-product-card 
+            title="Popular Pre-Built" 
+            description="High performance gaming rig." 
+            price="$1200"/>
+
+    <x-product-card 
+            title="Popular Pre-Built" 
+            description="High performance gaming rig." 
+            price="$1200"/>
+
+    <x-product-card 
+            title="Popular Pre-Built" 
+            description="High performance gaming rig." 
+            price="$1200"/>
+
+<x-product-card 
+            title="Popular Pre-Built" 
+            description="High performance gaming rig." 
+            price="$1200"/>
+</div>
--- resources/views/store.blade.php
+++ resources/views/store.blade.php
@@ -2,8 +2,92 @@
 <x-layout>
     <h1>Hello from the Store Page.</h1>
 </x-layout>
+<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 p-6">
+    <x-product-card 
+            title="Popular Pre-Built" 
+            description="High performance gaming rig." 
+            price="$1200"/>
+
+    <x-product-card 
+            title="Popular Pre-Built" 
+            description="High performance gaming rig." 
+            price="$1200"/>
+
+    <x-product-card 
+            title="Popular Pre-Built" 
+            description="High performance gaming rig." 
+            price="$1200"/>
+
 <x-product-card 
-          title="Popular Pre-Built" 
-          description="High performance gaming rig." 
-          price="$1200"/>
+            title="Popular Pre-Built" 
+            description="High performance gaming rig." 
+            price="$1200"/>
+</div>
+
+<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 p-6">
+    <x-product-card 
+            title="Popular Pre-Built" 
+            description="High performance gaming rig." 
+            price="$1200"/>
+
+    <x-product-card 
+            title="Popular Pre-Built" 
+            description="High performance gaming rig." 
+            price="$1200"/>
+
+    <x-product-card 
+            title="Popular Pre-Built" 
+            description="High performance gaming rig." 
+            price="$1200"/>
+
+<x-product-card 
+            title="Popular Pre-Built" 
+            description="High performance gaming rig." 
+            price="$1200"/>
+</div>
+
+<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 p-6">
+    <x-product-card 
+            title="Popular Pre-Built" 
+            description="High performance gaming rig." 
+            price="$1200"/>
+
+    <x-product-card 
+            title="Popular Pre-Built" 
+            description="High performance gaming rig." 
+            price="$1200"/>
+
+    <x-product-card 
+            title="Popular Pre-Built" 
+            description="High performance gaming rig." 
+            price="$1200"/>
+
+<x-product-card 
+            title="Popular Pre-Built" 
+            description="High performance gaming rig." 
+            price="$1200"/>
+</div>
+
+<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 p-6">
+    <x-product-card 
+            title="Popular Pre-Built" 
+            description="High performance gaming rig." 
+            price="$1200"/>
+
+    <x-product-card 
+            title="Popular Pre-Built" 
+            description="High performance gaming rig." 
+            price="$1200"/>
+
+    <x-product-card 
+            title="Popular Pre-Built" 
+            description="High performance gaming rig." 
+            price="$1200"/>
+
+<x-product-card 
+            title="Popular Pre-Built" 
+            description="High performance gaming rig." 
+            price="$1200"/>
+</div>
+
 <x-footer></x-footer>
