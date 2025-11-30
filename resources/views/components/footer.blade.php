48b10bfb121988e38eec8b2a3f9fc8cd1898374b
Samuel D'souza <samuel_dsouza@outlook.com>
2025-11-30 18:54:16 +0000

    fixed colour of footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,5 @@
 <!--The footer-->
-    <footer class="bg-gray-700 text-white border-t border-gray-700">
+    <footer class="bg-gray-800 text-white border-t border-gray-700">
         <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
             <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
                 
