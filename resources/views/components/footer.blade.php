eda5d87cd640334cd47087ecd7fea0324acdd97e
Samuel D'souza <samuel_dsouza@outlook.com>
2025-11-30 17:03:31 +0000

    fixed merge conflict in footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,4 @@
 <!--The footer-->
-<<<<<<< HEAD
-</html>
-=======
     <footer class="bg-gray-700 text-white border-t border-gray-700">
         <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
             <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
@@ -53,4 +50,3 @@
         </div>
     </footer>
 </html>
->>>>>>> 35de4b18d2009c264fce71611e6246717a21a6a4
