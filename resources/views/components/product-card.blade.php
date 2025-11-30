4639409fa3a2e2161cf13a0744a0a65e43409fc9
Samuel D'souza <samuel_dsouza@outlook.com>
2025-11-30 20:54:00 +0000

    removed unused basket html

--- resources/views/components/product-card.blade.php
+++ resources/views/components/product-card.blade.php
@@ -3,7 +3,7 @@
 
 <div class="bg-white p-6 rounded-lg shadow-sm flex flex-col items-center text-center">
     <div class="w-full bg-gray-200 h-48 rounded mb-6">
-        <img src={{$image}} double bracket here alt="product image"> This line causing errors
+        <img src={{$image}} double bracket here alt="product image">
     </div>
     <h3 class="text-xl font-bold mb-2">{{ $title }}</h3>
     <p class="text-gray-500 text-sm mb-6">{{ $description }}</p>
