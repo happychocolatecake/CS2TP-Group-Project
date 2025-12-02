fb9c9662bcdb9ba2675ba0b7c685ab9dd8175a1c
Aishah Hussain <anaishahhussain18@gmail.com>
2025-12-02 12:59:27 +0000

    fixed prices on shop page

--- resources/views/components/product-card.blade.php
+++ resources/views/components/product-card.blade.php
@@ -7,7 +7,7 @@
     </div>
     <h3 class="text-xl font-bold mb-2">{{ $title }}</h3>
     <p class="text-gray-500 text-sm mb-6">{{ $description }}</p>
-
+    <p class="text-gray-400 text-sm mb-6">£{{ $price }}</p>
     <button class="mt-auto bg-gray-800 text-white px-8 py-2 rounded hover:bg-gray-700 transition">
         Buy now
     </button>
