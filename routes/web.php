bda7ed43fa548a18709908f9823391bc9b6d115b
Aishah Hussain <anaishahhussain18@gmail.com>
2025-12-04 17:04:11 +0000

    fixed web.php error

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,8 @@ use Livewire\Volt\Volt;
 use Illuminate\Http\Request;
 use App\Http\Controllers\StoreController;
 use App\Http\Controllers\ProductController;
-use App\Http\Controllers\CheckoutController; 
-use Illuminate\Http\Request;
+use App\Http\Controllers\CheckoutController;
+use App\Http\Controllers\ProfileController;
 
 // --- Public Routes ---
 
@@ -59,11 +59,11 @@ Route::get('/basket', function () {
     if (request()->user()) {
         return redirect()->route('basket.view');
     }
-    
+
     return view('basket', ['basket' => null]);
 })->name('basket.guest');
 
-//  Checkout Routes 
+//  Checkout Routes
 Route::get('/checkout', [CheckoutController::class, 'index'])->name('checkout.index');
 Route::post('/checkout/process', [CheckoutController::class, 'processOrder'])->name('checkout.process');
 Route::get('/checkout/success', [CheckoutController::class, 'success'])->name('checkout.success');
@@ -86,11 +86,11 @@ Route::middleware(['auth'])->group(function () {
     Route::put('/profile/password', [ProfileController::class, 'updatePassword'])->name('profile.password');
     Route::get('/profile/orders', [ProfileController::class, 'orders'])->name('profile.orders');
     Route::get('/profile/security', [ProfileController::class, 'security'])->name('profile.security');
-    
-    // Password Change Page 
+
+    // Password Change Page
     Route::get('/password/change', [ProfileController::class, 'editPassword'])->name('password.edit');
     Route::put('/password/update', [ProfileController::class, 'updatePassword'])->name('profile.password.update');
-    
+
     Route::get('/profile/verify-email-change', [ProfileController::class, 'verifyEmailPage'])->name('profile.verify-email');
     Route::post('/profile/verify-email-change', [ProfileController::class, 'verifyEmailAction'])->name('profile.verify-email.store');
 
@@ -101,7 +101,7 @@ Route::middleware(['auth'])->group(function () {
 
 
     Volt::route('settings/two-factor', 'initial views.livewire.settings.two-factor')
-        ->middleware(['password.confirm']) 
+        ->middleware(['password.confirm'])
         ->name('two-factor.show');
 
 
