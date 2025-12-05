04397498cf60842924785776a2fed19b46500c4e
Aisha Jama <230186794@aston.ac.uk>
2025-12-05 02:35:33 +0000

    Changed the frontend of login and register page

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -1,5 +1,13 @@
+<x-layout>
+    <div class="flex items-center justify-center  bg-gradient-to-t from-gray-700 via-gray-800 to-gray-900">
 <x-layouts.auth>
-    <div class="flex flex-col gap-6">
+    <!-- Two flex box -->
+    <div class="flex justify-center items-stretch gap-6 ">
+
+        <!-- left flex box -->
+    <div class="flex justify-center gap-6 w-1/2 "> 
+    <div class=" border border-gray-700 bg-gray-100 rounded-3xl shadow-2xl p-8 flex flex-col justify-center h-full">
+    <div class="flex flex-col gap-6 ">
         <x-auth-header :title="__('Log in to your account')" :description="__('Enter your email and password below to log in')" />
 
         <!-- Session Status -->
@@ -56,4 +64,30 @@
             </div>
         @endif
     </div>
+</div>
+</div>
+        <div class="flex justify-center w-1/2 ">
+             <!-- right flex box-->
+        <div class="border border-gray-700 bg-gray-100 rounded-3xl shadow-2xl p-8 flex flex-col justify-start h-full">
+             <x-auth-header :title="__('Don\'t have an account?')" :description="__('Sign up now to get started!')" />
+        <br>
+            <p class="text-center text-black">{{ __('By signing up, you\'ll be able to access') }}</p>
+            <p class="text-center text-black">{{ __('you dashboard, manage your profile, ') }}</p>
+            <p class="text-center text-black">{{ __('and use all available features.') }}</p>
+        <br>
+            <p class="text-center text-black">{{ __('With an account, you can:') }}</p>
+            <p class="text-center text-black">{{ __('Save your order history') }}</p>
+            <p class="text-center text-black">{{ __('Receive updates and notifications') }}</p>
+            <p class="text-center text-black">{{ __('Sign up to enjoy everything we offer') }}</p>
+        <br><br>
+                    <flux:button variant="primary" type="button" class="w-full" wire:navigate :href="route('register')">
+                        {{ __('Sign up') }}
+                    </flux:button>
+                    </div>
+                </div>
+            </div>
+
 </x-layouts.auth>
+</div>
+<x-footer></x-footer>
+</x-layout>
