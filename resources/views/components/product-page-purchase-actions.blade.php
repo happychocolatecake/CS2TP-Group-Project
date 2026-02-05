81daecb064b202243bdf246623c37606f505c075
Aishah Hussain <anaishahhussain18@gmail.com>
2026-02-05 20:21:10 +0000

    Added working stock feature on product page

--- resources/views/components/product-page-purchase-actions.blade.php
+++ resources/views/components/product-page-purchase-actions.blade.php
@@ -15,6 +15,16 @@
                         <button type="button" id="plus_quantity" class= "hover:text-gray-600 pb-1 cursor-pointer"> + </button>
 
                     </div>
+                    <br>
+                    <div style= "font-weight: 500; font-size:20px; text-align:center">
+                        @if($stock >= 5)
+                            <p style="color: green; background:rgb(108, 249, 131)">In Stock</p>
+                        @elseif($stock > 0)
+                            <p style="color: rgb(180, 125, 25); background:rgb(241, 203, 132); padding-right:10px; padding-left:10px">Only {{$stock}} remaining</p>
+                        @else
+                            <p style="color: rgb(149, 30, 30); background:rgb(247, 143, 143)"> Out of stock</p>
+                        @endif
+                    </div>
                 </div>
             </div>
 
