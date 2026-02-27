999d3b8ec75233e3e9dbe991945541e8eb3d8f38
Aishah Hussain <anaishahhussain18@gmail.com>
2026-02-27 22:18:51 +0000

    Created a model for the review table and some seeders for account and reviews

---
+++ app/Models/Review.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    public $timestamps = false;
+
+    protected $fillable  = ['rating',
+    'review_image', 'review_text', 'review_date',
+    'user_id', 'order_id', 'product_id', 'created_at'];
+
+    //this is for the foreign keys (many to one)
+
+    public function user() {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function order() {
+        return $this->belongsTo(Order::class, 'order_id', 'id');
+    }
+
+    public function product() {
+        return $this->belongsTo(Product::class, 'product_id', 'id');
+    }
+
+}
---
+++ database/seeders/UserAccountSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        DB::table('products')->insert([
+            [
+
+            ]
+        ]);
+    }
+}
