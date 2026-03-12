660cd1f493080a91c181a7e9afebf427676f3a2a
Tom Thackham <thackhamt@gmail.com>
2026-03-12 14:41:45 +0000

    Fixing Footer Coloring

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -4,32 +4,37 @@
             <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
 
                 <div>
-                    <p class="text-sm text-gray-600 dark:text-gray-400">Let's stay in touch! Sign up to get the best deals!</p>
+                    <p class="text-sm text-gray-300 dark:text-gray-400">Let's stay in touch! Sign up to get the best deals!</p>
                     <div class="flex space-x-4 mt-2">
-                        <a href="#" class="text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">Instagram</a>
-                        <a href="#" class="text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">Facebook</a>
+                        <a href="#" class="text-gray-300 hover:text-white dark:text-gray-400 dark:hover:text-white">Instagram</a>
+                        <a href="#" class="text-gray-300 hover:text-white dark:text-gray-400 dark:hover:text-white">Facebook</a>
                     </div>
                 </div>
 
                 <div class="grid grid-cols-2 gap-4">
                     <div>
-                        <h3 class="text-sm font-semibold text-gray-300 tracking-wider uppercase">Help</h3>
+                        <h3 class="text-sm font-semibold text-gray-100 dark:text-gray-400 dark:hover:text-white tracking-wider uppercase">Help</h3>
                         <ul role="list" class="mt-4 space-y-4">
-                            <li><a href="faq" class="text-base text-gray-400 hover:text-white">FAQ</a></li>
-                            <li><a href="build-guide" class="text-base text-gray-400 hover:text-white">How-to Guides</a></li>
+                            <li><a href="faq" class="text-base text-gray-300 hover:text-white dark:text-gray-400 dark:hover:text-white">FAQ</a></li>
+                            <li><a href="build-guide" class="text-base text-gray-300 hover:text-white dark:text-gray-400 dark:hover:text-white">How-to Guides</a></li>
                         </ul>
                     </div>
                     <div>
-                        <h3 class="text-sm font-semibold text-gray-300 tracking-wider uppercase">Other</h3>
+                        <h3 class="text-sm font-semibold text-gray-100 dark:text-gray-400 dark:hover:text-white tracking-wider uppercase">Other</h3>
                         <ul role="list" class="mt-4 space-y-4">
-                            <li><a href="privacy" class="text-base text-gray-400 hover:text-white">Privacy Policy</a></li>
-                            <li><a href="#" class="text-base text-gray-400 hover:text-white">Sitemap</a></li>
+                            <li><a href="privacy" class="text-base text-gray-300 hover:text-white dark:text-gray-400 dark:hover:text-white">Privacy Policy</a></li>
+                            <li><a href="#" class="text-base text-gray-300 hover:text-white dark:text-gray-400 dark:hover:text-white">Sitemap</a></li>
                         </ul>
                     </div>
                 </div>
             </div>
+
+            <div>
+                <br>
+            </div>
+
             <div class="mt-4 border-t border-gray-300 pt-4 md:flex md:items-center md:justify-between dark:border-gray-700">
-                <p class="text-sm text-gray-600 dark:text-gray-400">&copy; 2025 Happy Hardware. Group 27.</p>
+                <p class="text-sm text-gray-300 dark:text-gray-400">&copy; 2025 Happy Hardware. Group 27.</p>
             </div>
         </div>
     </footer>
