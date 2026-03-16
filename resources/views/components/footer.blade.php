0efb7af45af3b6100a9153afacd3bbb0efb382c4
Aishah Hussain <anaishahhussain18@gmail.com>
2026-03-16 19:24:24 +0000

    fixed footer darkmode

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -4,10 +4,10 @@
             <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
 
                 <div>
-                    <p class="text-sm text-gray-600 dark:text-gray-400">Let's stay in touch! Sign up to get the best deals!</p>
+                    <p class="text-sm text-gray-300">Let's stay in touch! Sign up to get the best deals!</p>
                     <div class="flex space-x-4 mt-2">
-                        <a href="#" class="text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">Instagram</a>
-                        <a href="#" class="text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">Facebook</a>
+                        <a href="#" class="text-gray-400 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">Instagram</a>
+                        <a href="#" class="text-gray-400 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">Facebook</a>
                     </div>
                 </div>
 
@@ -28,8 +28,8 @@
                     </div>
                 </div>
             </div>
-            <div class="mt-4 border-t border-gray-300 pt-4 md:flex md:items-center md:justify-between dark:border-gray-700">
-                <p class="text-sm text-gray-600 dark:text-gray-400">&copy; 2026 Happy Hardware. Group 27.</p>
+            <div class="mt-4 border-t pt-4 md:flex md:items-center md:justify-between border-gray-700">
+                <p class="text-sm text-gray-400">&copy; 2026 Happy Hardware. Group 27.</p>
             </div>
         </div>
     </footer>
