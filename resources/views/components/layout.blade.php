92bad496f9054ce68b4444ca118342b5aa00b18c
Aisha <85460366+aishajama1@users.noreply.github.com>
2026-03-16 23:36:28 +0000

    added basket item count to the navbar

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -56,10 +56,13 @@
                             </svg>
                         </a>
 
-                        <a href="/basket" class="rounded-full p-2 hover:bg-gray-100 transition-all duration-200 dark:hover:bg-white/10">
+                        <a href="/basket" class="relative rounded-full p-2 hover:bg-gray-100 transition-all duration-200 dark:hover:bg-white/10">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6 text-gray-600 hover:text-gray-900 transition-colors duration-200 dark:text-gray-400 dark:hover:text-white">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
                             </svg>
+                            <span class="absolute -top-1 -right-1 bg-red-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center"> 
+                                0
+                            </span> 
                         </a>
                     </div>
 
