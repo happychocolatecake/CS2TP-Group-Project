1a2e95a36759731e5135537c68988af40d9d93b9
Aisha <85460366+aishajama1@users.noreply.github.com>
2026-03-18 22:29:59 +0000

    Improved on the footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,10 +1,14 @@
-<footer class="bg-gray-800 text-white border-t border-gray-700 transition-colors duration-300">
+<footer class="bg-gray-800 text-white bg-gradient-to-b from-gray-800 to-gray-900 rounded-t-3xl transition-colors duration-300">
     <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
 
-        <div class="flex flex-row justify-center gap-16 sm:gap-32">
+        <div class="flex flex-row justify-center gap-24 sm:gap-40">
+            <div class="px-4">
+                <h2 class="text-xl font-semibold text-white"> Happy Hardware </h2>
+                <p class="text-gray-400 text-sm mt-2"> Quality hardeware at the best prices. </p>
+</div>
 
             <div class="w-auto">
-                <h3 class="text-sm font-semibold text-gray-100 tracking-wider uppercase dark:text-gray-400">
+                <h3 class="text-sm font-semibold text-gray-100 tracking-wider uppercase dark:text-gray-100">
                     <strong>HELP</strong>
                 </h3>
                 <ul role="list" class="mt-4 space-y-4">
@@ -22,17 +26,17 @@
             </div>
 
             <div class="w-auto">
-                <h3 class="text-sm font-semibold text-gray-100 tracking-wider uppercase dark:text-gray-400">
+                <h3 class="text-sm font-semibold text-gray-100 tracking-wider uppercase dark:text-gray-100">
                     <strong>OTHER</strong>
                 </h3>
                 <ul role="list" class="mt-4 space-y-4">
                     <li>
-                        <a href="privacy" class="text-base text-gray-300 hover:text-white transition-colors duration-200 dark:text-gray-400 dark:hover:text-white">
+                        <a href="privacy" class="text-base text-gray-400 hover:text-white transition-colors duration-200 dark:text-gray-400 dark:hover:text-white">
                             Privacy Policy
                         </a>
                     </li>
                     <li>
-                        <a href="#" class="text-base text-gray-300 hover:text-white transition-colors duration-200 dark:text-gray-400 dark:hover:text-white">
+                        <a href="#" class="text-base text-gray-400 hover:text-white transition-colors duration-200 dark:text-gray-400 dark:hover:text-white">
                             Sitemap
                         </a>
                     </li>
@@ -43,7 +47,7 @@
 
         <div class="mt-12 border-t border-gray-700 pt-8 text-center">
             <p class="text-sm text-gray-400">
-                &copy; 2025 Happy Hardware. Group 27.
+                &copy; 2026 Happy Hardware. Group 27.
             </p>
         </div>
     </div>
