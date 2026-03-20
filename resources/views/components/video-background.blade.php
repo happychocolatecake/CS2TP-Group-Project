fcf5dfb3170847fb610f123ecc40fb6e3af89928
Jot17 <harjotsingh4167@gmail.com>
2026-03-20 01:09:55 +0000

    decreased opacity and increase size of background image

--- resources/views/components/video-background.blade.php
+++ resources/views/components/video-background.blade.php
@@ -14,27 +14,29 @@
     <div class="absolute inset-0 bg-slate-100 transition-colors duration-700 ease-in-out dark:bg-slate-950"></div>
 
     <div
-        class="absolute inset-0 opacity-65 transition-opacity duration-700 ease-in-out dark:opacity-0"
+        class="absolute inset-0 opacity-40 transition-opacity duration-700 ease-in-out dark:opacity-0"
         style="
             background-image: url('{{ asset('images/mouseBackground.png') }}');
             background-repeat: repeat;
-            background-size: 240px 240px;
-            animation: diagonalMouseDrift 24s linear infinite;
+            background-size: 320px 320px;
+            animation: diagonalMouseDrift 40s linear infinite;
             will-change: background-position, opacity;
         "
     ></div>
 
     <div
-        class="absolute inset-0 opacity-0 transition-opacity duration-700 ease-in-out dark:opacity-[0.58]"
+        class="absolute inset-0 opacity-0 transition-opacity duration-700 ease-in-out dark:opacity-[0.34]"
         style="
             background-image: url('{{ asset('images/mouseBackgroundBlack.png') }}');
             background-repeat: repeat;
-            background-size: 240px 240px;
-            animation: diagonalMouseDrift 24s linear infinite;
+            background-size: 320px 320px;
+            animation: diagonalMouseDrift 40s linear infinite;
             will-change: background-position, opacity;
         "
     ></div>
 
-    <div class="absolute inset-0 bg-white/18 transition-colors duration-700 ease-in-out dark:bg-slate-950/16"></div>
-    <div class="absolute inset-0 bg-gradient-to-b from-transparent via-white/6 to-white/10 transition-colors duration-700 ease-in-out dark:via-slate-950/6 dark:to-slate-950/12"></div>
+    <div class="absolute inset-0 bg-white/28 transition-colors duration-700 ease-in-out dark:bg-slate-950/24"></div>
+    <div class="absolute inset-0 bg-gradient-to-b from-transparent via-white/12 to-white/18 transition-colors duration-700 ease-in-out dark:via-slate-950/12 dark:to-slate-950/22"></div>
 </div>
+
+
