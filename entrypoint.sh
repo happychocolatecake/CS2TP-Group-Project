71e789bb4e30707cd8f8e6162c61ba6fe08c9d9a
Aishah <anaishahhussain18@gmail.com>
2026-05-31 22:08:42 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # running some shell code here as i cant run script in render manually
+php artisan config:clear
+php artisan cache:clear
+php artisan view:clear
 
 php artisan migrate --force
 
